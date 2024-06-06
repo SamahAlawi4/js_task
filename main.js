4ff1eaf372b1cb13e626583bd298e9857288db9e
SamahAllawi2 <samah.alawi4@icloud.com>
2024-06-06 21:16:52 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var btn1=document.querySelector(".font_change");
+function change(){
+  var changeBtn=document.querySelector("h3");
+  changeBtn.classList.add("font_change");
+
+}
+btn1.onclick=change;
+
+
+
+
+  
